b171addc7f068ecaa7363089df5e605463a40f07
parksakeun <arch2boy@naver.com>
2025-05-01 17:03:59 +0900

    Create CommanderMovement.cs

---
+++ Assets/Scripts/Assets/Scripts/Assets/Scripts/CommanderMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CommanderMovement : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
